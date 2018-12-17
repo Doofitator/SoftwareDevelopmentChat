9e7edb56e6383e92a3171686e4f6733380d24136
Doofitator <ashley@sharkey.id.au>
2018-12-17 19:12:54 +1100

    Added comment module so we can talk

---
+++ SoftwareDevelopmentChat/Comments.vb
@@ -0,0 +1,10 @@
+﻿Module Comments
+    'HUGARIAN NOTATION:
+    '   Format: btn_sendToServer (notation, underscore, name with seporate words distinguished by capital letters)
+    '   btn -> button
+    '   lbl -> label
+    '   txt -> textbox
+    '   rbtn -> radio button
+    '   chk -> checkbox
+    '   grp -> groupbox
+End Module
