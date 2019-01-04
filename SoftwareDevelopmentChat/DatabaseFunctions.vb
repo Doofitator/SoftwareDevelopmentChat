634c266379e9edf508bc85948186cd874475e09b
Doofitator <ashley@sharkey.id.au>
2019-01-05 09:50:48 +1100

    Added function to database module

--- SoftwareDevelopmentChat/DatabaseFunctions.vb
+++ SoftwareDevelopmentChat/DatabaseFunctions.vb
@@ -1,3 +1,11 @@
 ﻿Module DatabaseFunctions
     'This module will contain all functions that are used to talk to the database, from sending messages to sending login credentials
+
+    Public DatabaseConnection = New OleDb.OleDbConnection
+    Public newMedia As System.IO.FileInfo = Nothing
+
+    Function connect()
+        DatabaseConnection.ConnectionString = "Provider=Microsoft.ACE.OLEDB.16.0;Data Source=http://marist.vic.edu.au/showcase/vce_soft_dev/chat/SoftDevChat.accdb"
+        DatabaseConnection.Open()
+    End Function
 End Module
