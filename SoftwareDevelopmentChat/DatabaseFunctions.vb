6747cb26d255606d1b4886bc3e1872d3b8e4866c
Doofitator <ashley@sharkey.id.au>
2019-01-06 17:12:36 +1100

    Internal documentation

--- SoftwareDevelopmentChat/DatabaseFunctions.vb
+++ SoftwareDevelopmentChat/DatabaseFunctions.vb
@@ -33,7 +33,8 @@ Module DatabaseFunctions
     End Function
 
     Public Function MakeSQLSafe(ByVal sql As String) As String ' Ripped this function from https://stackoverflow.com/questions/725367/how-to-filter-out-some-vulnerability-causing-characters-in-query-string.
-        '                                                        TODO: Need to make it more efficient and handle illegal characters better (not remove them, but make them safe).
+        '                                                        TODO: Need to make it more efficient and handle illegal characters better (not remove them, but make them safe) to avoid people's passwords 
+        '                                                              changing here without them knowing & then Not being able to get back in.
 
         '// HENRY: If you're confused as to why this is here and what it does, check out https://www.youtube.com/watch?v=_jKylhJtPmI //
 
@@ -65,14 +66,13 @@ Module DatabaseFunctions
         myConn.Open()
 
         Try
-            Dim reader As SqlDataReader = myCmd.ExecuteReader()
-            If reader.HasRows Then
-
+            Dim reader As SqlDataReader = myCmd.ExecuteReader() 'new reader
+            If reader.HasRows Then 'if the reader contains anything
                 While reader.Read()
-                    Console.WriteLine("{0}" & vbTab & "{1}", reader.GetInt32(0), reader.GetString(1))
+                    Console.WriteLine("{0}" & vbTab & "{1}", reader.GetInt32(0), reader.GetString(1)) 'write it to the console
                 End While
             Else
-                Console.WriteLine("No rows found.")
+                Console.WriteLine("No rows found.") 'reader contains nothing: write message to console.
             End If
             myConn.Close()
         Catch ex As Exception
