7f58db737a3bd7a56c3acaf88e569cd567178f80
Doofitator <ashley@sharkey.id.au>
2019-01-08 14:45:13 +1100

    Existing messages now load onclick of stream button, but aren't pretty

--- SoftwareDevelopmentChat/DatabaseFunctions.vb
+++ SoftwareDevelopmentChat/DatabaseFunctions.vb
@@ -16,21 +16,21 @@ Module DatabaseFunctions
 
     Function writeSQL(ByVal Query As String) As Boolean 'with any luck, all database WRITE functions will call this one at some point.
         'Create a Connection object.
-        myConn = New SqlConnection(connectionString)
+        Dim MyConn = New SqlConnection(connectionString)
 
         'Create a Command object.
-        myCmd = myConn.CreateCommand
+        Dim myCmd = MyConn.CreateCommand
         myCmd.CommandText = Query
 
         'Open the connection.
-        myConn.Open()
+        MyConn.Open()
 
         Try
             myCmd.ExecuteNonQuery() 'run sql script
-            myConn.Close() 'close connection
+            MyConn.Close() 'close connection
             Return True
         Catch ex As Exception 'if a catastrophic error occurs
-            myConn.Close() 'close the connection
+            MyConn.Close() 'close the connection
             errorInfo = ex 'make error information publicly available
             Return False
         End Try
@@ -38,20 +38,20 @@ Module DatabaseFunctions
 
     Function userExists(ByVal name As String) As Boolean
         'Create a Connection object.
-        myConn = New SqlConnection(connectionString)
+        Dim MyConn = New SqlConnection(connectionString)
 
         'Create a Command object.
-        myCmd = myConn.CreateCommand
+        Dim myCmd = MyConn.CreateCommand
         myCmd.CommandText = "select count(*) from tbl_users where convert(varchar, Name) = '" & name & "'"
 
         'Open the connection.
-        myConn.Open()
+        MyConn.Open()
 
         If myCmd.ExecuteScalar = 1 Then 'if there is one result returned, then the username already exists in the database.
-            myConn.Close()
+            MyConn.Close()
             Return True
         Else
-            myConn.Close()
+            MyConn.Close()
             Return False
         End If
     End Function
@@ -61,25 +61,25 @@ Module DatabaseFunctions
         Dim sql2 As String = "select count(*) from tbl_streams where convert(varchar, StreamName) = '" & user2 & " and " & user1 & "'"
 
         'Create a Connection object.
-        myConn = New SqlConnection(connectionString)
+        Dim MyConn = New SqlConnection(connectionString)
 
         'Create a Command object.
-        myCmd = myConn.CreateCommand
+        Dim myCmd = MyConn.CreateCommand
         myCmd.CommandText = sql1 'set command to check sql1
 
         'Open the connection.
-        myConn.Open()
+        MyConn.Open()
 
         If myCmd.ExecuteScalar = 1 Then 'if there is one result returned, then the stream already exists in the database.
-            myConn.Close()
+            MyConn.Close()
             Return True 'sql1 exists
         Else
             myCmd.CommandText = sql2 'set command to sql2
             If myCmd.ExecuteScalar = 1 Then
-                myConn.Close()
+                MyConn.Close()
                 Return True 'sql2 exists
             Else
-                myConn.Close()
+                MyConn.Close()
                 Return False ' neither exist
             End If
         End If
@@ -87,14 +87,14 @@ Module DatabaseFunctions
 
     Function readUserPassword(ByVal Name As String) As String 'function to read passwords from database.
         'Create a Connection object.
-        myConn = New SqlConnection(connectionString)
+        Dim MyConn = New SqlConnection(connectionString)
 
         'Create a Command object.
-        myCmd = myConn.CreateCommand
+        Dim myCmd = MyConn.CreateCommand
         myCmd.CommandText = "select Password from tbl_users where convert(varchar, Name) = '" & Name & "'"
 
         'Open the connection.
-        myConn.Open()
+        MyConn.Open()
 
         Dim result As String = "False" 'this is what the function will return
 
@@ -103,11 +103,11 @@ Module DatabaseFunctions
             While reader.Read
                 result = reader.GetString(0) 'get first value of field (because there should only be one record returned as there shouldn't be username doubleups).
             End While
-            myConn.Close() 'close connection
+            MyConn.Close() 'close connection
             Dim decryptor As New Encryption(Name) 'new instance of encryption module
             result = decryptor.DecryptData(result) 'decript with username key
         Catch ex As Exception 'if a catastrophic error occurs
-            myConn.Close() 'close the connection
+            MyConn.Close() 'close the connection
             errorInfo = ex
             Return "False"
         End Try
@@ -117,14 +117,14 @@ Module DatabaseFunctions
 
     Function readUserID(ByVal Name As String) As Integer 'function to read IDs from database.
         'Create a Connection object.
-        myConn = New SqlConnection(connectionString)
+        Dim MyConn = New SqlConnection(connectionString)
 
         'Create a Command object.
-        myCmd = myConn.CreateCommand
+        Dim myCmd = MyConn.CreateCommand
         myCmd.CommandText = "select ID from tbl_users where convert(varchar, Name) = '" & Name & "'"
 
         'Open the connection.
-        myConn.Open()
+        MyConn.Open()
 
         Dim result As Integer = 0 'this is what the function will return
 
@@ -133,9 +133,9 @@ Module DatabaseFunctions
             While reader.Read
                 result = reader.GetInt32(0) 'get first value of field (because there should only be one record returned as there shouldn't be username doubleups).
             End While
-            myConn.Close() 'close connection
+            MyConn.Close() 'close connection
         Catch ex As Exception 'if a catastrophic error occurs
-            myConn.Close() 'close the connection
+            MyConn.Close() 'close the connection
             errorInfo = ex
             Return 0
         End Try
@@ -147,14 +147,14 @@ Module DatabaseFunctions
         Dim streams As New List(Of String)
 
         'Create a Connection object.
-        myConn = New SqlConnection(connectionString)
+        Dim MyConn = New SqlConnection(connectionString)
 
         'Create a Command object.
-        myCmd = myConn.CreateCommand
+        Dim myCmd = MyConn.CreateCommand
         myCmd.CommandText = "select StreamName from tbl_streams Where streamName like '%" & frm_main.txt_userName.Text & "%'" 'select streamname where it includes your name
 
         'Open the connection.
-        myConn.Open()
+        MyConn.Open()
 
         Dim result As String = "False" 'this is what the function will return
 
@@ -165,10 +165,10 @@ Module DatabaseFunctions
                 streams.Add(CType(reader("StreamName"), String)) 'add streamname to list as string
             End While
 
-            myConn.Close() 'close connection
+            MyConn.Close() 'close connection
             Return streams.ToArray
         Catch ex As Exception 'if a catastrophic error occurs
-            myConn.Close() 'close the connection
+            MyConn.Close() 'close the connection
             errorInfo = ex
             Return False
         End Try
@@ -179,14 +179,15 @@ Module DatabaseFunctions
         Dim messages As New List(Of String)
 
         'Create a Connection object.
-        myConn = New SqlConnection(connectionString)
+        Dim MyConn = New SqlConnection(connectionString)
 
         'Create a Command object.
-        myCmd = myConn.CreateCommand
-        myCmd.CommandText = "select Message from tbl_messages Where StreamID like '%" & MakeSQLSafe(readStreamID(frm_main.grp_chat.Text)) & "%'" 'select message where it includes the stream ID
+        Dim myCmd = MyConn.CreateCommand
+        myCmd.CommandText = "select Message from tbl_messages Where StreamID = '" & MakeSQLSafe(readStreamID(frm_main.grp_chat.Text)) & "'" 'select message where it includes the stream ID
+        Console.WriteLine(myCmd.CommandText)
 
         'Open the connection.
-        myConn.Open()
+        MyConn.Open()
 
         Dim result As String = "False" 'this is what the function will return
 
@@ -197,10 +198,10 @@ Module DatabaseFunctions
                 messages.Add(CType(reader("Message"), String)) 'add message to list as string
             End While
 
-            myConn.Close() 'close connection
+            MyConn.Close() 'close connection
             Return messages.ToArray
         Catch ex As Exception 'if a catastrophic error occurs
-            myConn.Close() 'close the connection
+            MyConn.Close() 'close the connection
             errorInfo = ex
             Return False
         End Try
@@ -209,10 +210,10 @@ Module DatabaseFunctions
 
     Function readStreamID(ByVal streamName As String) As Integer 'returns 0 on fail
         'Create a Connection object.
-        myConn = New SqlConnection(connectionString)
+        Dim MyConn = New SqlConnection(connectionString)
 
         'Create a Command object.
-        myCmd = myConn.CreateCommand
+        Dim myCmd = myConn.CreateCommand
         myCmd.CommandText = "select StreamID from tbl_streams where convert(varchar, StreamName) = '" & streamName & "'"
 
         'Open the connection.
