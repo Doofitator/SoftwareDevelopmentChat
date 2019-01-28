c82606b13726daf0dc906b22107d35b817be07a5
Doofitator <ashley@sharkey.id.au>
2019-01-28 12:41:03 +1100

    update todos

--- SoftwareDevelopmentChat/Comments.vb
+++ SoftwareDevelopmentChat/Comments.vb
@@ -3,8 +3,6 @@
 
     'TODO: full scale test (see how database copes with lots of data)
 
-    'TODO: swear/javascript injection blocker
-
     'TODO: classes
 
     'TODO: admin rights
@@ -14,8 +12,6 @@
     'TODO: webbrowsers need to dynamically size
 
     'TODO: two windows need to be better in sync. Minimise one, minimise the other. Activate one, the other appears next to it.
-
-    'TODO: since adding pnl_messages, resising is kind of broken
 
     'TODO: If one person sends 'hi'. and three days later somebody else in the chat sends 'hi', the whole thing crashes because it can't work out who sent each 'hi' message :/
 End Module
