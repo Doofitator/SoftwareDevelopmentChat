df1964dc556447664842a65d2d644df160c5b45a
Doofitator <ashley@sharkey.id.au>
2019-01-30 19:46:52 +1100

    found bug

--- SoftwareDevelopmentChat/Comments.vb
+++ SoftwareDevelopmentChat/Comments.vb
@@ -14,4 +14,6 @@
     'TODO: If one person sends 'hi'. and three days later somebody else in the any chat sends 'hi', the whole thing crashes because it can't work out who sent each 'hi' message :/
 
     'TODO: Who sent the group chat message?
+
+    'TODO: HTML broke the chat download :(
 End Module
