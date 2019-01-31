65e4305e1242c7576d6ddfd02c5187a293434dc6
Doofitator <ashley@sharkey.id.au>
2019-01-31 18:15:14 +1100

    edited todos

--- SoftwareDevelopmentChat/Comments.vb
+++ SoftwareDevelopmentChat/Comments.vb
@@ -9,8 +9,6 @@
 
     'TODO: RecipientRead not updating
 
-    'TODO: two windows need to be better in sync. Minimise one, minimise the other. Activate one, the other appears next to it.
-
     'TODO: If one person sends 'hi'. and three days later somebody else in the any chat sends 'hi', the whole thing crashes because it can't work out who sent each 'hi' message :/
 
     'TODO: Who sent the group chat message?
